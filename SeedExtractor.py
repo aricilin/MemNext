b11fe6f044696b6e560d7e0955dc472a1433daba
Sting <lallegre26@gmail.com>
2023-02-15 22:36:46 +0100

    Declare API entrypoints + autodoc

---
+++ SeedExtractor.py
@@ -0,0 +1,47 @@
+
+
+class SeedExtractor:
+
+
+    class Seed:
+
+        def __init__(self):
+            """
+            Constructor
+
+            """
+
+
+    def __init__(self):
+        """
+        Constructor
+
+        """
+
+    def extract(self, inputText):
+        """
+        Extracts a list of seeds from given text.
+
+        :param inputText: Text to extract seeds from.
+        :type inputText: str
+        :return: A list of seeds.
+        :rtype: list[Seed]
+
+        """
+
+    def write(self, seedList, file, format):
+        """
+        Write a list of seeds to a file in the specified format
+
+        :param seedList: Seeds to write
+        :type seedList: list[Seed]
+        :param file: File to write to
+        :type file: str
+        :param format: File format
+        :type inputText: str
+        :return: None
+        :rtype: None
+
+        """
+
+    
