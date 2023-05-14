9f73a0579bb67aa56d11e3a43110529824366411
lorenzoblaise <lorenzoblaise@gmail.com>
2023-05-15 00:05:34 +0200

    ouput seedmarker-like + affichage web

--- NameExtract/Spacy.py
+++ NameExtract/Spacy.py
@@ -1,55 +1,63 @@
 import spacy
 from spacy import displacy
 import sys
-from spacy.lang.fr import  French
+from spacy.lang.fr import French
 import webbrowser
+import nltk
+from spacy.tokens import Doc
+nltk.download('punkt')
 
-#check arg number
+# check arg number
 if (len(sys.argv) != 2):
     print("utilisation : NameExtractSpacy.py texte_source")
     exit()
 
 # load the pre-trained languages model
 
-#nlp=spacy.load('fr_core_news_sm')# fr rapide extraction nom echec
+# nlp=spacy.load('fr_core_news_sm')# fr rapide extraction nom echec
 # nlp=spacy.load('fr_dep_news_trf') # fr lent extraction nom echec
 
 # nlp = spacy.load("en_core_web_trf") #eng lent résultat bon
-#nlp = spacy.load('en_core_web_sm') # eng rapide résultat mauvais
+# nlp = spacy.load('en_core_web_sm') # eng rapide résultat mauvais
 
-#nlp = spacy.load("fr_dep_news_trf")
+# nlp = spacy.load("fr_dep_news_trf")
 config = spacy.Config().from_disk("../ModelTraining/config.cfg")
-nlp = French.from_config(config) 
+nlp = French.from_config(config)
 nlp.from_disk("../ModelTraining/output/model-best")
 # define some text to analyze
-text= open(sys.argv[1],"r",encoding="utf-8")
+text = open(sys.argv[1], "r", encoding="utf-8")
 
-# use spaCy's NER module to identify named entities in the text
-doc = nlp(text.read())
+filepath = sys.argv[1]
+filename = filepath.split('/')[len(filepath.split('/'))-1]
+filename = f"output/{filename}"
 
-#if an entity is not in nameList and its label is 'PERSON' print it
-nameList =[]
-nb=0
+tuplelist = []
+outputlist=[]
+sentences = nltk.sent_tokenize(text.read())
 
-# for entity in doc.ents:
-#     try:
-#         if entity.text not in nameList:
-#             print(entity.text, entity.label_)
-#             nameList.append(entity.text)
-#             nb+=1
-#     except UnicodeEncodeError:
-#         continue
-# print (nb, "lignes")
 
-#seeds colors option
-colors = {"0": "#F3F4ED", "1": "#F28482", "2": "#96BB7C", "3": "#76b5c5", "4": "#abdbe3", "5": "#D6EFC7", "6": "#F5CAC3", "7": "#7D1F35", "8": "#158467", "9": "#22577A"}
-options = { "colors": colors}
+listdoc=[]
+for sentence in sentences:
+    doc = nlp(sentence)
+    for ent in doc.ents:
+        tuple = (ent.start_char, ent.end_char, ent.label_)
+        tuplelist.append(tuple)
+    listdoc.append(doc)
+    outputlist.append((sentence,tuplelist))
+    tuplelist=[]
 
-#auto opening of web browser
+fulldoc= Doc.from_docs(listdoc)
+
+with open(filename, "w", encoding="utf-8") as f:
+    f.write(str(outputlist))
+
+# seeds colors option
+colors = {"0": "#F3F4ED", "1": "#F28482", "2": "#96BB7C", "3": "#76b5c5", "4": "#abdbe3",
+          "5": "#D6EFC7", "6": "#F5CAC3", "7": "#7D1F35", "8": "#158467", "9": "#22577A"}
+options = {"colors": colors}
+
+# auto opening of web browser
 url = "http://localhost:5000/"
 webbrowser.open(url)
 
-server=displacy.serve(doc, style="ent", options=options)
-
-
-
+server = displacy.serve(fulldoc, style="ent", options=options)
