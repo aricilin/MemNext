ae7e53c53b2c023e24d15e650fc5209a543e618b
lorenzoblaise <lorenzoblaise@gmail.com>
2023-05-16 12:24:28 +0200

    retirage de la graine but

--- ModelTraining/preprocess.py
+++ ModelTraining/preprocess.py
@@ -2,10 +2,10 @@ import spacy
 from spacy.tokens import DocBin
 import ast
 
-nlp = spacy.blank("fr") 
+nlp = spacy.blank("fr")
 
 
-with open("entrainement.txt", 'r',encoding="utf-8") as file:
+with open("entrainement.txt", 'r', encoding="utf-8") as file:
     # Read the contents of the file
     contents = file.read()
     # Split the contents by the delimiter (e.g., comma)
@@ -15,18 +15,18 @@ with open("entrainement.txt", 'r',encoding="utf-8") as file:
 
 # the DocBin will store the example documents
 db = DocBin()
-pt=0
+pt = 0
 for text, annotations in training_data:
     doc = nlp(text)
     ents = []
     for start, end, label in annotations:
-        try: 
-            span = doc.char_span(start, end, label=label)
-            ents.append(span)
-            doc.ents = ents
-        except TypeError: # error  ex tag cut a word
-            pt+=1
-            continue
+        if label not in ["9"]:
+            try:
+                span = doc.char_span(start, end, label=label)
+                ents.append(span)
+                doc.ents = ents
+            except TypeError:  # error  ex tag cut a word
+                print(f"({start}, {end}, '{label}')")
+                exit
     db.add(doc)
-db.to_disk("./train.spacy") 
-print (pt)
+db.to_disk("./train.spacy")
